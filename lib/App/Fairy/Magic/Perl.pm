fb377c1f2253a6f766bb68a77932e606189da5dd
Petr Šabata <contyk@redhat.com>
2015-09-23 18:04:41 +0200

    Add a quick and dirty CPAN index updater
    Signed-off-by: Petr Šabata <contyk@redhat.com>

--- lib/App/Fairy/Magic/Perl.pm
+++ lib/App/Fairy/Magic/Perl.pm
@@ -1,6 +1,33 @@
 package App::Fairy::Magic::Perl;
 use strict;
 use warnings;
+use File::HomeDir;
+use File::Path ();
+use File::Spec;
+use HTTP::Request;
+use LWP::UserAgent;
+
+sub _update_cpan_index {
+    my $index = '02packages.details.txt.gz';
+    my $indexpath = File::Spec->catfile(
+        File::HomeDir->my_home, qw/.cpan sources modules/, $index);
+    my $indexurl =
+        ($ENV{CPAN} || "http://www.cpan.org/") . "/modules/${index}";
+    File::Path->make_path((File::Spec->splitpath($indexpath))[1]);
+    my $ua = LWP::UserAgent->new(env_proxy => 1);
+    my $req = HTTP::Request->new(GET => $indexurl);
+    $req->if_modified_since((stat($indexpath))[9]);
+    my $res = $ua->request($req);
+    if ($res->is_success) {
+        open my $fh, '>', $indexpath;
+        print $fh $res->content;
+        close $fh;
+        utime(time, $res->last_modified, $indexpath);
+    } else {
+        return unless $res->code eq '304';
+    }
+    return 1;
+}
 
 1;
 
