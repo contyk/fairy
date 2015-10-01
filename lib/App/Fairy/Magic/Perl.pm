50ed4eed8bda4dee1ad161525efc159a7a4956e4
Petr Šabata <contyk@redhat.com>
2015-10-01 14:28:21 +0200

    Extract distribution name from the index
    Prefer Parse::CPAN::Packages::Fast since it's really, well, fast.
    About five times faster than the non-fast variant.  And they should
    be fully compatible for this use case.
    
    Signed-off-by: Petr Šabata <contyk@redhat.com>

--- lib/App/Fairy/Magic/Perl.pm
+++ lib/App/Fairy/Magic/Perl.pm
@@ -1,17 +1,50 @@
 package App::Fairy::Magic::Perl;
 use strict;
 use warnings;
+use App::Fairy::Meta;
 use App::Fairy::Utils qw/fetch/;
 use File::HomeDir;
 use File::Spec;
 
+my $index = '02packages.details.txt.gz';
+my $indexpath = File::Spec->catfile(
+    File::HomeDir->my_home, qw/.cpan sources modules/, $index);
+my $indexurl =
+    ($ENV{CPAN} || "http://www.cpan.org/") . "/modules/${index}";
+
 sub _update_cpan_index {
-    my $index = '02packages.details.txt.gz';
-    my $indexpath = File::Spec->catfile(
-        File::HomeDir->my_home, qw/.cpan sources modules/, $index);
-    my $indexurl =
-        ($ENV{CPAN} || "http://www.cpan.org/") . "/modules/${index}";
     fetch($indexurl, $indexpath);
+}
+
+sub _get_distribution {
+    my $token = shift;
+    my $packages;
+    eval {
+        require Parse::CPAN::Packages::Fast;
+        $packages = Parse::CPAN::Packages::Fast->new($indexpath);
+        1;
+    } or do {
+        require Parse::CPAN::Packages;
+        $packages = Parse::CPAN::Packages->new($indexpath);
+    };
+    $packages
+        or die "CPAN index parsing failure.\n";
+    my $p = $packages->package($token)
+        or die "Cannot find `$token' in the CPAN index.\n";
+    my $d = $p->distribution
+        or die "Cannot map `$token' to any CPAN distribution.\n";
+    return $d;
+}
+
+sub new {
+    return bless {}, shift;
+}
+
+sub do {
+    my ($self, $spell) = @_;
+    _update_cpan_index();
+    my $dist = _get_distribution($spell);
+    print $dist->dist."\n".$dist->version."\n";
 }
 
 1;
